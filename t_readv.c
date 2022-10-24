3ae2f5512340bbe835ceca2d3d3f8718d5df8e64
adelaide <481744093@qq.com>
2022-10-24 15:57:28 +0800

    Add some extra parameters.

--- t_readv.c
+++ t_readv.c
@@ -8,4 +8,8 @@ int main(int argc, char **argv)
 	printf("hello, don't be sad(^v^) \n");
 	int fd;
 	struct iovec iov[3];
+	struct stat myStruct;
+	int x;
+#define STR_SIZE 100
+	char str[STR_SIZE];
 }
